1e9f2ffa81c2ca7f43a8c00ace9f39a764de1244
DahyeLee1205 <106331443+DahyeLee1205@users.noreply.github.com>
2023-01-26 12:05:49 +0900

    삭제

--- src/main/java/com/milk/product/controller/MainProductController.java
+++ src/main/java/com/milk/product/controller/MainProductController.java
@@ -1,28 +1,23 @@
-package com.milk.common.controller;
+package com.milk.product.controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.milk.product.model.service.ProductService;
-import com.milk.product.model.vo.Product;
-
 /**
- * Servlet implementation class MainContentController
+ * Servlet implementation class MainProductController
  */
 @WebServlet("/iLoveMilk")
-public class MainContentController extends HttpServlet {
+public class MainProductController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public MainContentController() {
+    public MainProductController() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -31,10 +26,8 @@ public class MainContentController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		ArrayList<Product> p = new ProductService().selectRecentProductList();
-		
-		
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
