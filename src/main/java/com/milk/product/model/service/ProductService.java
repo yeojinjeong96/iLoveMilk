f06846df517ed557162d30bf77aa6c4c6dc56d0a
moonseungha <115603472+moonseungha@users.noreply.github.com>
2023-01-26 10:11:59 +0900

    productInsert

--- src/main/java/com/milk/product/model/service/ProductService.java
+++ src/main/java/com/milk/product/model/service/ProductService.java
@@ -3,11 +3,8 @@ package com.milk.product.model.service;
 import static com.milk.common.JDBCTemplate.*;
 
 import java.sql.Connection;
-import java.util.ArrayList;
 
-import com.milk.common.model.vo.PageInfo;
 import com.milk.product.model.dao.ProductDao;
-import com.milk.product.model.vo.Product;
 
 public class ProductService {
 	
@@ -15,18 +12,6 @@ public class ProductService {
 		
 		Connection conn = getConnection();
 		int listCount = new ProductDao().selectListCount(conn, category);
-		
-		close(conn);
-		return listCount;
-		
-	}
-	
-	public ArrayList<Product> selectProductList(PageInfo pi, String category){
-		
-		Connection conn = getConnection();
-		ArrayList<Product> list = new ProductDao().selectProductList(conn, pi, category);
- 		
-		return list;
 		
 	}
 
